227de4c12345a0cdfaa320fd27cb09054c46a365
Lucho <Lbgonzalez@alumno.huergo.edu.ar>
2025-09-23 21:02:59 -0300

    Create Carta.java

---
+++ detectivesoft/src/main/java/ar/edu/huergo/gorodriguez/detectivesoft/entity/cartas/Carta.java
@@ -0,0 +1,50 @@
+package ar.edu.huergo.gorodriguez.detectivesoft.entity.cartas;
+
+import jakarta.persistence.Column; 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue; 
+import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table; 
+import lombok.Data; 
+import lombok.NoArgsConstructor; 
+import lombok.AllArgsConstructor;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import ar.edu.huergo.gorodriguez.detectivesoft.entity.jugador.Jugador;
+import ar.edu.huergo.gorodriguez.detectivesoft.entity.partida.Partida;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cartas")
+public class Carta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TipoCarta tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "partida_id", nullable = false)
+    private Partida partida;
+
+    @ManyToOne
+    @JoinColumn(name = "jugador_id")
+    private Jugador jugador;
+
+    public enum TipoCarta {
+        PERSONAJE,
+        ARMA,
+        HABITACION
+    }
+}
