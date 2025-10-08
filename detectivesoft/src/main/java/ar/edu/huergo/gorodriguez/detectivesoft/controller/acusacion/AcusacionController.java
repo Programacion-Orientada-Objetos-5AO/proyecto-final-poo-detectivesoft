d0bf94113ed731581a402713e9568c8381a0ebd9
gonza9LAN <gonzarodri2007@gmail.com>
2025-10-08 12:14:43 -0300

    Create AcusacionController.java

---
+++ detectivesoft/src/main/java/ar/edu/huergo/gorodriguez/detectivesoft/controller/acusacion/AcusacionController.java
@@ -0,0 +1,60 @@
+package ar.edu.huergo.gorodriguez.detectivesoft.controller.acusacion;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.huergo.gorodriguez.detectivesoft.dto.acusacion.AcusacionDto;
+import ar.edu.huergo.gorodriguez.detectivesoft.dto.security.MensajeDto;
+import ar.edu.huergo.gorodriguez.detectivesoft.service.acusacion.AcusacionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/acusaciones")
+
+public class AcusacionController {
+    
+    private final AcusacionService acusacionService;
+
+    //Crear Acusacion
+    @PostMapping
+    public ResponseEntity<AcusacionDto> crearAcusacion(@Valid @RequestBody AcusacionDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(acusacionService.crearAcusacion(dto));
+    }
+
+    // Obtener todas las acusaciones
+    @GetMapping
+    public ResponseEntity<List<AcusacionDto>> obtenerTodas() {
+        return ResponseEntity.ok(acusacionService.obtenerTodas());
+    }
+
+    // Obtener acusaciones por partida
+    @GetMapping("/partida/{partidaId}")
+    public ResponseEntity<List<AcusacionDto>> obtenerPorPartida(@PathVariable("partidaId") Long partidaId) {
+        return ResponseEntity.ok(acusacionService.obtenerPorPartida(partidaId));
+    }
+
+    // Obtener acusaciones por jugador
+    @GetMapping("/jugador/{jugadorId}")
+    public ResponseEntity<List<AcusacionDto>> obtenerPorJugador(@PathVariable("jugadorId") Long jugadorId) {
+        return ResponseEntity.ok(acusacionService.obtenerPorJugador(jugadorId));
+    }
+
+    //Eliminar Acusacion
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MensajeDto> eliminarAcusacion(@PathVariable("id") Long id){
+        acusacionService.eliminarAcusacion(id);
+        return ResponseEntity.ok(new MensajeDto("Acusacion eliminada correctamente."));
+    }
+}
